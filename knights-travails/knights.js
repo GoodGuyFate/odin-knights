9da6ae52b82f84872bc9d20c9748d6343b6d4613
GoodGuyFate <xgamerfatex@gmail.com>
2026-02-25 08:17:12 +0200

    Implement knightMoves and testing

--- knights-travails/knights.js
+++ knights-travails/knights.js
@@ -1,3 +1,51 @@
-export function knightsMoves() {
+export function knightMoves(start, end) {
+  let queue = [{ position: start, path: [start] }];
+  let visited = new Set();
+  visited.add(start.join(","));
 
+  while (queue.length > 0) {
+    const { position, path } = queue.shift();
+
+    if (position[0] === end[0] && position[1] === end[1]) {
+      return path;
+    }
+
+    const nextMoves = getLegalMoves(position);
+
+    nextMoves.forEach((move) => {
+      const moveStr = move.join(",");
+      if (!visited.has(moveStr)) {
+        visited.add(moveStr);
+        queue.push({
+          position: move,
+          path: [...path, move],
+        });
+      }
+    });
+  }
 }
+
+const isWithinBoard = (x, y) => {
+  return x >= 0 && x <= 7 && y >= 0 && y <= 7;
+};
+
+const getLegalMoves = ([x, y]) => {
+  const offsets = [
+    [2, 1],
+    [2, -1],
+    [-2, 1],
+    [-2, -1],
+    [1, 2],
+    [1, -2],
+    [-1, 2],
+    [-1, -2],
+  ];
+
+  return offsets
+    .map(([dx, dy]) => [x + dx, y + dy])
+    .filter(([nx, ny]) => isWithinBoard(nx, ny));
+};
+
+const path = knightMoves([3,3], [3,4]);
+console.log(`You made it in ${path.length - 1} moves! Here's your path:`)
+path.forEach(pos => console.log(pos));
--- knights-travails/knights.test.js
+++ knights-travails/knights.test.js
@@ -1,1 +1,31 @@
-import { knightsMoves } from "./knights";
+import { knightMoves } from "./knights";
+import { isWithinBoard } from "./knights"
+
+describe("knightMoves", () => {
+  test("should return the start square if start and end are the same", () => {
+    expect(knightMoves([0, 0], [0, 0])).toEqual([[0, 0]]);
+  });
+
+  test("should find a path for a single move", () => {
+    const path = knightMoves([0, 0], [1, 2]);
+    expect(path).toEqual([
+      [0, 0],
+      [1, 2],
+    ]);
+  });
+
+  test("should find the shortest path from [0, 0] to [3, 3]", () => {
+    const path = knightMoves([0, 0], [3, 3]);
+    // path could be [[0,0], [1,2], [3,3]] or [[0,0], [2,1], [3,3]]
+    expect(path.length).toBe(3);
+    expect(path[0]).toEqual([0, 0]);
+    expect(path[path.length - 1]).toEqual([3, 3]);
+  });
+
+  test("should find a path from one corner to the opposite corner", () => {
+    const path = knightMoves([0, 0], [7, 7]);
+    // usually takes 6 moves (7 squares total)
+    expect(path.length).toBe(7);
+  });
+});
+
